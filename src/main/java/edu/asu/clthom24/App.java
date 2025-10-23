9fb9651cd0c220e2493b63adeb5f76a4f4f4634d
Chandler <clthom24@asu.com>
2025-10-22 19:42:49 -0700

    Initial Maven project commit

---
+++ src/main/java/edu/asu/clthom24/App.java
@@ -0,0 +1,13 @@
+package edu.asu.clthom24;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
